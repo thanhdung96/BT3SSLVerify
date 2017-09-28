9685fe4f2bb6388902d4d58f032d13385f9e495c
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-09-28 22:13:17 +0700

    Init Project

---
+++ BT3SSL_UDP_Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace BT3SSL_UDP_Client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
